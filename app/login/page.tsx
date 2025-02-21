73a3df6230bc4abaf0474313a91caee318afad16
saivenkatsharma <saivenkatsharma3@gmail.com>
2025-02-21 12:32:23 +0530

    Simplify login page with default student login redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function LoginPage() {
-<<<<<<< Updated upstream
-  return <LoginForm />
-=======
   // Redirect to student login by default
   redirect('/login/student')
->>>>>>> Stashed changes
 } 
